e44a20e5ed15782bb2cfb6db52af98fa24c7916b
IInfo <i-in-fo@qq.com>
2021-03-25 18:53:59 +0800

    push

--- socketservice.cpp
+++ socketservice.cpp
@@ -2,5 +2,42 @@
 
 SocketService::SocketService(QObject *parent) : QObject(parent)
 {
+    isConnected = false;
+    isThreadStoped = false;
+}
 
+SocketService::~SocketService()
+{
+    delete tcpSocket;
+}
+
+void SocketService::startSocket(const QString &ip, const int &port)
+{
+    this->serverIp = ip;
+    this->serverPort = port;
+    tcpSocket = new QTcpSocket();
+    isConnected = false;
+    isThreadStoped = false;
+    //Link
+    connect(tcpSocket,SIGNAL(connected()),this,SLOT(onConnected()));
+    connect(tcpSocket,SIGNAL(disConnected()),this,SLOT(onDisconnected()));
+    connect(tcpSocket,SIGNAL(readyRead()),this,SLOT(onReadMsg()));
+    run();
+}
+
+void SocketService::stopSocket()
+{
+    tcpSocket->disconnectFromHost();
+}
+
+void SocketService::run()
+{
+    while (!isThreadStoped) {
+        if (!isConnected) {
+            tcpSocket->connectToHost(this->serverIp,this->serverPort);
+            isConnected = tcpSocket->waitForConnected();
+
+        }
+
+    }
 }
--- socketservice.h
+++ socketservice.h
@@ -2,14 +2,37 @@
 #define SOCKETSERVICE_H
 
 #include <QObject>
+#include <QTcpSocket>
+
+class QTcpSocket;
 
 class SocketService : public QObject
 {
     Q_OBJECT
 public:
     explicit SocketService(QObject *parent = nullptr);
+    ~SocketService();
+    void startSocket(const QString& ip, const int& port);
+    void stopSocket();
 
 signals:
+    void error();
+    void remoteMsg();
+
+
+private slots:
+    void onConnected();
+    void onDisconnected();
+    void onReadMsg();
+
+
+private:
+    QTcpSocket *tcpSocket;
+    bool isConnected;
+    bool isThreadStoped;
+    QString serverIp;
+    int serverPort;
+    void run();
 
 };
 
